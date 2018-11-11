cc205fbbc7b114683f1c0c21e0dc02a2bb8908e8
Tran Quang Loc <quangloctran99@gmail.com>
2018-11-11 17:22:26 +0300

    Create CF80-D2-D.py

---
+++ Codeforces/CF80-D2-D.py
@@ -0,0 +1,15 @@
+# Author: Tran Quang Loc (darkkcyan)
+# Editorial: http://codeforces.com/blog/entry/1773
+def calArea(a, b):
+    if a <= 4 * b:
+        return a * min(b, a / 4) / 2
+    return b * b * 2 + (a - 4 * b) * b 
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    if b == 0:
+        print(1)
+    elif a == 0:
+        print(0.5)
+    else:
+        print("{:.8f}".format(calArea(a, b) / (2 * a * b) + 0.5))
