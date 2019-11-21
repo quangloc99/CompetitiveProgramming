26160bcb8e6eb774791220480360460f90acfeba
Tran Quang Loc <quangloctran99@gmail.com>
2019-11-21 23:30:38 +0300

    Change `llong` to `long long`

--- UVA/10104.cpp
+++ UVA/10104.cpp
@@ -15,18 +15,18 @@ using namespace std::placeholders;
     // return (T)(rng() % range);
 // }
 
-tuple<llong, llong, llong> extended_gcd(llong a, llong b) {
+tuple<long long, long long, long long> extended_gcd(long long a, long long b) {
     if (b == 0) return {a, 1, 0};
-    llong gcd, x1, y1;                              // 2 lines can be written as one in C++17
+    long long gcd, x1, y1;                          // 2 lines can be written as one in C++17
     tie(gcd, x1, y1) = extended_gcd(b, a % b);      // auto [gcd, x1, y1] = extended_gcd(b, a % b);
     return {gcd, y1, x1 - (a / b) * y1};
 }
 
 int main(void) {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    llong a, b;
+    long long a, b;
     while (cin >> a >> b) {
-        llong gcd, x, y;
+        long long gcd, x, y;
         tie(gcd, x, y) = extended_gcd(a, b);
         cout << x << ' ' << y << ' ' << gcd << '\n';
     }
