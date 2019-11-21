988a0e3285b8478c1eb0c84eebabf3185ccdde9e
Tran Quang Loc <quangloctran99@gmail.com>
2019-11-21 23:30:55 +0300

    Change `llong` to `long long`

--- prepared-code/math/extended_gcd.h
+++ prepared-code/math/extended_gcd.h
@@ -3,9 +3,9 @@
  * Tested with UVA 10104.
  * Code sample https://github.com/quangloc99/CompetitiveProgramming/blob/master/UVA/10104.cpp
  */
-tuple<llong, llong, llong> extended_gcd(llong a, llong b) {
+tuple<long long, long long, long long> extended_gcd(long long a, long long b) {
     if (b == 0) return {a, 1, 0};
-    llong gcd, x1, y1;                              // 2 lines can be written as one in C++17
+    long long gcd, x1, y1;                          // 2 lines can be written as one in C++17
     tie(gcd, x1, y1) = extended_gcd(b, a % b);      // auto [gcd, x1, y1] = extended_gcd(b, a % b);
     return {gcd, y1, x1 - (a / b) * y1};
 }
