11e665143fb6e6d5fe8898baa01bec4aabd08aba
Tran Quang Loc <quangloctran99@gmail.com>
2019-11-22 10:21:37 +0300

    Change my `len` macro into `#size` method.

--- prepared-code/string/kmp.cpp
+++ prepared-code/string/kmp.cpp
@@ -6,7 +6,7 @@
 template<class container>
 vector<int> cal_lsp(const container& s) {
   vector<int> ans(1, 0);
-  for (int cur_len = 0; len(ans) < len(s); ) {
+  for (int cur_len = 0; ans.size() < s.size(); ) {
     if (s[len(ans)] == s[cur_len]) ans.push_back(++cur_len);
     else if (cur_len) cur_len = ans[cur_len - 1];
     else ans.push_back(0);
