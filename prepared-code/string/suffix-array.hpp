9e5556b371edd676219ca936a4d253770fc24ee5
Tran Quang Loc <quangloctran99@gmail.com>
2019-11-25 00:00:26 +0300

    Add function for calculating lcp array.

--- prepared-code/string/suffix-array.hpp
+++ prepared-code/string/suffix-array.hpp
@@ -1,6 +1,7 @@
 /**
  * Author: Tran Quang Loc (darkkcyan)
  * Tested with https://github.com/quangloc99/CompetitiveProgramming/blob/master/SPOJ/SARRAY.cpp
+ *        and  https://github.com/quangloc99/CompetitiveProgramming/blob/master/SPOJ/ADASTRNG.cpp
  */
 inline void counting_sort_pos(vector<int>& pos, function<int(int)> get_key, size_t alphabet) {
     vector<int> cnt(alphabet), old_pos = pos;
@@ -27,4 +28,22 @@ vector<int> construct_suffarr(const container& s) {
             rank[pos[i]] = rank[pos[i - 1]] + (keys[pos[i - 1]] != keys[pos[i]]);
     }
     return pos;
+}
+
+template<class container>
+vector<int> construct_lcp(const container& s, const vector<int> sa) {
+    int n = (int)s.size();
+    vector<int> lcp(n - 1), inv(n);
+    for (int i = n; i--; ) inv[sa[i]] = i;
+
+    for (int i = 0, k = 0; i < n; ++i) {
+        if (inv[i] == n - 1) {
+            k = 0;
+            continue;
+        }
+        for (int f = sa[inv[i] + 1]; i + k < n and f + k < n and s[i + k] == s[f + k]; ++k);
+        lcp[inv[i]] = k;
+        if (k) --k;
+    }
+    return lcp;
 }
